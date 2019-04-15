d51838be3e5c81547b4758172d2c1654bd10fc86
KrisQ <kquemener@yahoo.com>
2019-04-15 15:11:23 -0500

    last test

--- commission_ssibtr/__manifest__.py
+++ commission_ssibtr/__manifest__.py
@@ -21,6 +21,7 @@
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'product'],
 
+
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
